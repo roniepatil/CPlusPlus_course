7d952e9cc473ea40fc4c2d0198c8fa37bc032a56
Rohit M Patil <roniepatil@gmail.com>
2025-07-01 22:41:38 -0400

    July1 2ndCommit

--- cincout.cpp
+++ cincout.cpp
@@ -7,21 +7,68 @@ int main() {
     //cout << (Insertion operator)
     //cin >> (Extraction operator)
 
+    // std::string name;
+    // std::cout << "What is your name ?" << '\n';
+    // std::cin >> name;
+
+    /*
+    we can use std::getline(std::cin, name) if input has any white space in between
+    
     std::string name;
     std::cout << "What is your name ?" << '\n';
-    std::cin >> name;
+    std::getline(std::cin, name);
+
+    int age;
+    std::cout << "What is your age ?" << '\n';
+    std::cin >> age;
+    
+    */
+
+   /*
+   Above works fine until std::cin >> age is moved above std::getline()
+   For Example:
+
+    int age;
+    std::cout << "What is your age ?" << '\n';
+    std::cin >> age;
+
+    std::string name;
+    std::cout << "What is your name ?" << '\n';
+    std::getline(std::cin, name);
+
+    In the line ""std::cin >> age;"" there is input buffer with new line character,
+    which we dont pickup, when we reach the getline, it accepts the new line character that
+    still within the buffer
+
+    NOW USE TO FIX ABOVE:
+    std::getline(std::cin >> std::ws, name);
+   
+   */
     
     
 
     // If you hit space in user input it will truncate upto first space
     // So use below method
     std::string fullName;
+    int age;
+
+
+    std::cout << "Whats your age?" << '\n';
+    std::cin >> age;
+    
+    
     std::cout << "What's your full name ?" << '\n';
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore remaining characters in the stream
+
+
+
+
+    // OLD WAY
+    // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore remaining characters in the stream
+    // MODERN WAY
     std::getline(std::cin >> std::ws, fullName); // std::ws ignores leading whitespace in fullName
 
-    std::cout << "Hello " << name << ", How are you doing today?" << "\n";
-    std::cout << "Hello " << fullName << ", How are you doing?" << '\n';
+    // std::cout << "Hello " << name << ", How are you doing today?" << "\n";
+    std::cout << "Hello " << fullName << ", You are " << age << " years old!"<< '\n';
 
 
 
--- typedef.cpp
+++ typedef.cpp
@@ -4,8 +4,10 @@
 typedef std::vector<std::pair<std::string, int>> pairlist_t;
 // typedef variables end with _t to denote type definition
 typedef std::string text_t;
+// typedef bool bool_t;
 // typedef int number_t;
 using number_t = int;
+using bool_t = bool;
 
 int main(){
 
@@ -17,11 +19,12 @@ int main(){
 
     pairlist_t pair;
     text_t name = "rob";
-
+    bool_t student = false;
 
     number_t age = 23; 
 
     std::cout << name << " is " << age << '\n';
+    std::cout << "Is he a student: " << std::boolalpha << student << '\n';
     
 
 
