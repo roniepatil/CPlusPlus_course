4e82db9be31574997a33ea8e228917c9cdad6609
Rohit M Patil <roniepatil@gmail.com>
2025-07-01 23:18:22 -0400

    July 1st 3rd commit

--- ifstatements.cpp
+++ ifstatements.cpp
@@ -9,6 +9,9 @@ int main() {
     std::cout << "What's your age?" << '\n';
     std::cin >> age;
 
+    // if statements = do something if a condition is true.
+    //                 if not, then don't do it.
+
     if(age >= 18){
         std::cout << "You can enter this site!" << '\n';
     }
