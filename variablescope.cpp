eb2a0b0ec1fcbd65d2c00961be254c61a54039f4
Rohit M Patil <roniepatil@gmail.com>
2025-07-14 23:50:32 -0400

    July 14th 1st commit

---
+++ variablescope.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include <ctime>
+
+
+int main() {
+
+    // Local variables = declared inside a function or block {}
+    // Global variables = declared outside of all functions
+
+    
+
+    return 0;
+
+}
