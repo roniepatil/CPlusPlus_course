5fa06b1835062c4359a3f96030097a91a18c1875
Rohit M Patil <roniepatil@gmail.com>
2025-07-17 23:07:21 -0400

    July 17th 2nd commit

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,6 +5,25 @@
 
 void bubbleSort(int array[], int size);
 
+/*
+Bubble sort is a simple sorting algorithm that repeatedly steps through 
+the list, compares adjacent elements, and swaps them if they are in the 
+wrong order. The pass through the list is repeated until the list is 
+sorted. The algorithm gets its name because smaller or larger elements 
+"bubble" to their correct position in the list.
+
+Completing a pass: After the first full pass, the largest element in the 
+array will have "bubbled up" to the very end.
+
+Repeating the process: The algorithm then starts over from the beginning, 
+but since the last element is already in its correct place, it only needs to check up to the second-to-last element. This process is repeated for each element in the array.
+
+Each pass through the array places the next largest element in its proper 
+place. For an array of n elements, the algorithm will need to perform n-1 
+passes to guarantee the array is sorted.
+
+*/
+
 int main() {
 
     int array[] = {9, 5, 8, 4, 6, 2, 1, 3, 7, 10};
