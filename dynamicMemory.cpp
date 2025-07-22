3b9a68df04c513b218b4bd5a25adf404b6a5445c
Rohit M Patil <roniepatil@gmail.com>
2025-07-21 23:52:09 -0400

    July 21st 2nd commit

---
+++ dynamicMemory.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include <ctime>
+
+
+int main() {
+
+    /*
+        dynamic memory = Memory that is allocated after the program
+                         is already compiled & running.
+                         Use the 'new' operator to allocate
+                         memory in the heap rather than the stack
+
+                         Useful when we don't know how much memory
+                         we will need. Makes our programs more fexible
+                         especially when accepting user input.
+    
+    */
+
+    
+
+    return 0;
+
+}
