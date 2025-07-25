7537b206f1feb294aee502546b3dfc2b910b5b04
Rohit M Patil <roniepatil@gmail.com>
2025-07-24 23:34:36 -0400

    July 24th 2nd commit

---
+++ oop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include <ctime>
+
+
+int main() {
+
+    // object = A collection of attributes and methods
+    //          They can have characteristics and could perform actions
+    //          Can be used to mimic real world items (ex. Phone, Book, Dog)
+    //          Created from a class which acts as a "blue-print"
+
+    
+
+
+    return 0;
+
+}
