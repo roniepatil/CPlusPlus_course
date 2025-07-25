56a47d9be01b5752345161043eab72304ae5006b
Rohit M Patil <roniepatil@gmail.com>
2025-07-24 23:53:52 -0400

    July 24th 3rd commit

--- oop.cpp
+++ oop.cpp
@@ -3,6 +3,23 @@
 #include <cmath>
 #include <ctime>
 
+class Car{
+    public:
+        std::string make = "Dodge";
+        std::string model;
+        int year;
+        std::string color;
+
+
+        void accelerate(){
+            std::cout << "Car is accelerating!" << '\n';
+        };
+
+        void brake(){
+            std::cout << "Car is braking!" << '\n';
+        };
+
+};
 
 int main() {
 
@@ -11,7 +28,33 @@ int main() {
     //          Can be used to mimic real world items (ex. Phone, Book, Dog)
     //          Created from a class which acts as a "blue-print"
 
-    
+    Car car1;
+
+    car1.model = "Challenger";
+    car1.year = 2023;
+    car1.color = "Black";
+
+    Car car2;
+
+    car2.make = "Ford";
+    car2.model = "Mustang";
+    car2.year = 2024;
+    car2.color = "Grey";
+
+    std::cout << car1.make << '\n';
+    std::cout << car1.model << '\n';
+    std::cout << car1.year << '\n';
+    std::cout << car1.color << '\n';
+
+    std::cout << '\n';
+    std::cout << car2.make << '\n';
+    std::cout << car2.model << '\n';
+    std::cout << car2.year << '\n';
+    std::cout << car2.color << '\n';
+
+    std::cout << '\n';
+    car1.accelerate();
+    car1.brake();
 
 
     return 0;
